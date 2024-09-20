a128748107efb3e3617eec45c4557b69b436e84f
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-09-20 23:04:18 +0530

    added ai-icon on likedIn messagebox

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,33 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
+  matches: ["https://www.linkedin.com/*"],
+
   main() {
-    console.log('Hello content.');
+    const observer = new MutationObserver(() => {
+      const messageBox = document.getElementsByClassName(
+        "msg-form__contenteditable",
+      )[0];
+
+      if (messageBox && !messageBox.querySelector(".sticky-icon")) {
+        const stickyDiv = document.createElement("div");
+        const stickyIcon = document.createElement("img");
+
+        stickyIcon.src = chrome.runtime.getURL("/icon/ai-icon.svg");
+        stickyIcon.alt = "Icon";
+        stickyIcon.height = 32;
+        stickyIcon.width = 32;
+        stickyIcon.classList.add("sticky-icon");
+
+        stickyDiv.style.position = "absolute";
+        stickyDiv.style.bottom = "0px";
+        stickyDiv.style.right = "6px";
+        stickyDiv.style.zIndex = "1000";
+        stickyDiv.style.cursor = "pointer";
+
+        stickyDiv.appendChild(stickyIcon);
+        messageBox.appendChild(stickyDiv);
+      }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
   },
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  manifest: {
+    web_accessible_resources: [
+      {
+        matches: ["https://www.linkedin.com/*"],
+        resources: ["/icon/ai-icon.svg"],
+      },
+    ],
+  },
 });
