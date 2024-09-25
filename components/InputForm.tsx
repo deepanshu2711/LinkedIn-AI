6bebb2427a9548f6fbd7f5578376ae7801ebbca0
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-09-26 00:20:27 +0530

    completed regenerate and insert

--- components/InputForm.tsx
+++ components/InputForm.tsx
@@ -5,7 +5,11 @@ type Message = {
   from: "user" | "model";
 };
 
-export const InputForm = () => {
+interface InputFormProps {
+  TogglePromptDialog: (isOpen: boolean) => void;
+}
+
+export const InputForm = ({ TogglePromptDialog }: InputFormProps) => {
   const [prompt, setPrompt] = useState<string>("");
   const [messages, setMessages] = useState<Message[]>([]);
 
@@ -22,9 +26,48 @@ export const InputForm = () => {
     setPrompt("");
   };
 
+  const handleReGenerateOutput = (e: FormEvent) => {
+    e.preventDefault();
+
+    setMessages([
+      ...messages,
+      {
+        text: "Thank you for the opportunity! If you have any more questions or if there's anything else I can help you with, feel free to ask.",
+        from: "model",
+      },
+    ]);
+  };
+
+  const handleInsert = (e: FormEvent) => {
+    e.preventDefault();
+
+    const messageBox = document.getElementsByClassName(
+      "msg-form__contenteditable",
+    )[0];
+
+    if (messageBox) {
+      const PTag = messageBox.querySelector("p");
+
+      if (PTag) {
+        PTag.textContent =
+          "Thank you for the opportunity! If you have any more questions or if there's anything else I can help you with, feel free to ask.";
+      }
+
+      const placeholder = document.getElementsByClassName(
+        "msg-form__placeholder",
+      )[0];
+
+      if (placeholder) {
+        placeholder.removeAttribute("data-placeholder");
+      }
+
+      TogglePromptDialog(false);
+    }
+  };
+
   return (
     <div className="w-full flex flex-col">
-      <div className="flex flex-col w-full gap-4 mb-5">
+      <div className="flex flex-col w-full gap-4 mb-5 max-h-[200px] overflow-y-auto">
         {messages &&
           messages.length > 0 &&
           messages.map((message) => (
@@ -67,7 +110,7 @@ export const InputForm = () => {
         ) : (
           <div className="flex items-center justify-end gap-4">
             <button
-              onClick={handleGenerateOutput}
+              onClick={handleInsert}
               className="bg-transparent border text-gray-500 flex items-center gap-[10px] py-1  font-medium px-3 rounded-md"
             >
               <img
@@ -80,7 +123,7 @@ export const InputForm = () => {
               <p>Insert</p>
             </button>
             <button
-              onClick={handleGenerateOutput}
+              onClick={handleReGenerateOutput}
               className="bg-[#3B82F6] text-white border flex items-center gap-[10px] py-1 font-medium px-3 rounded-md"
             >
               <img
--- components/promptDialog.tsx
+++ components/promptDialog.tsx
@@ -1,38 +1,44 @@
+import { useRef, useEffect } from "react";
+
 interface PromptDialogProps {
   openDialog: boolean;
   TogglePromptDialog: (isOpen: boolean) => void;
 }
+
 export const PromptDialog = ({
   openDialog,
   TogglePromptDialog,
 }: PromptDialogProps) => {
   const dialogRef = useRef<HTMLDivElement | null>(null);
 
-  // useEffect(() => {
-  //   const handleClickOutside = (event: MouseEvent) => {
-  //     if (
-  //       dialogRef.current &&
-  //       !dialogRef.current.contains(event.target as Node)
-  //     ) {
-  //       TogglePromptDialog(false);
-  //     }
-  //   };
-  //
-  //   document.addEventListener("mousedown", handleClickOutside);
-  //   return () => {
-  //     document.removeEventListener("mousedown", handleClickOutside);
-  //   };
-  // }, []);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dialogRef.current &&
+        !dialogRef.current.contains(event.target as Node)
+      ) {
+        TogglePromptDialog(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [TogglePromptDialog]);
 
   if (!openDialog) return null;
+
   return (
     <div className="flex items-center justify-center h-full">
       <div className="bg-black opacity-50 fixed top-0 bottom-0 left-0 right-0 z-[1000]" />
       <div
         ref={dialogRef}
-        className="bg-[#F9FAFB] w-[60vh] flex p-5 flex-col items-end justify-end z-[1001] fixed top-1/2 left-[48%] transform -translate-x-1/2 -translate-y-1/2  rounded-lg shadow-lg"
+        className="bg-[#F9FAFB] w-[60vh] flex p-5 flex-col items-end justify-end z-[1001] fixed top-1/2 left-[48%] transform -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-lg"
+        onMouseDown={(e) => e.stopPropagation()}
       >
-        <InputForm />
+        <InputForm TogglePromptDialog={TogglePromptDialog} />
       </div>
     </div>
   );
